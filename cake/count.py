61782a4fe25903017db3aced4d53c2ea7e5d3f5d
Cheesecake <kani.kengatharan@datawizpro.com>
2026-05-23 10:44:41 -0400

    1a

---
+++ cake/count.py
@@ -0,0 +1,13 @@
+# Create a for loop that counts backwards from 10 to 1
+for i in range(10,0,-1):
+    print(i)
+
+
+
+
+
+# Do the same question but with a while loop
+count = 10
+while count >= 1:
+    print(count)
+    count -= 1
