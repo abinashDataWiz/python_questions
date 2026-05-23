0effaa1485cabfbbd96e6da4e23144f558910e5c
Abinash <abinash.nagendran@datawizpro.com>
2026-05-23 09:58:15 -0400

    moved to anshi folder

--- questions/for-loops/count.py
+++ questions/for-loops/count.py
@@ -1,11 +1,3 @@
 # Create a for loop that counts backwards from 10 to 1
 
-for i in range (10,0,-1):
-    print (i)
-    
-print("\n\n\n\n")
 # Do the same question but with a while loop
-w = 10
-while w >= 1:
-    print(w)
-    w = w - 1
