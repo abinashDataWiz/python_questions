fb054399d5b19ef5d36254a87e18f7bd7f9fccaa
anshi 10 <anshi.sabesan@datawizpro.com>
2026-05-30 09:50:08 -0400

    q answer

--- anshi's folder/find_number.py
+++ anshi's folder/find_number.py
@@ -1,9 +1,16 @@
 nums = [2,3,4,5,6,7,8,9,]
 
-target = 5
+target = 3
 
 # Print out the index of the target in nums using a for loop
 
-for i in range (len(nums)):
- if nums[i] == 5:
-    print(i)
+# for i in range (len(nums)):
+#  if nums[i] == 5:
+#     print(i)
+
+i = 0
+
+while i < len(nums) and nums[i] != target:
+     i = i + 1
+if i < len(nums):
+     print(i)
