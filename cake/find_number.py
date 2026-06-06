ea6006ad8a5e64835ebcfec8f91e15c9b6f635d0
Cheesecake <kani.kengatharan@datawizpro.com>
2026-06-06 10:32:36 -0400

    h

---
+++ cake/find_number.py
@@ -0,0 +1,10 @@
+nums = [2,3,4,5,6,7,8,9,]
+
+target = 7
+
+# Print out the index of the target in nums using a for loop
+for i in range(len(nums)):
+    if nums[i] == target:
+        print(i)
+
+#print(nums.index(67))
